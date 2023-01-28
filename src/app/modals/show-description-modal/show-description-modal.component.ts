e10fe06694a7934f5138c10b5d9ec315cdafdaee
Salomé Gfeller <salomegf.sg@gmail.com>
2023-01-28 17:49:01 +0100

    petits changements de mise en page

--- src/app/modals/show-description-modal/show-description-modal.component.ts
+++ src/app/modals/show-description-modal/show-description-modal.component.ts
@@ -5,45 +5,39 @@ import { Place } from '../../models/place'
 
 import { ModalController } from '@ionic/angular';
 
-
-
 @Component({
   selector: 'app-show-description-modal',
   templateUrl: './show-description-modal.component.html',
   styleUrls: ['./show-description-modal.component.scss'],
 })
 
-
-
 export class ShowDescriptionModalComponent implements OnInit {
-  
-  
   // @Input() public place;
 
   //places: Place[];
   data: Place;
-  
-  constructor(private modalCtrl: ModalController) {  }
 
-  ngOnInit() { 
-    
-    console.log(this.data)
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+
+    //console.log(this.data)
     // console.log(this.place)
     // this.placeService.getPlaces().subscribe(place => {
     //       this.places = place;
     //       //console.log(this.places)
     //       this.places.forEach(place => {
 
-                
+
     //       this.selectedPlaces = place;
-                
+
     //             // console.log(this.selectedPlaces)
     //           }
     //     )})
 
 
     // console.log(this.selectedPlaces)
-    
+
   }
 
   cancel() {
@@ -60,15 +54,13 @@ export class ShowDescriptionModalComponent implements OnInit {
   //     //console.log(this.places)
   //     this.places.forEach(place => {
 
-        
+
   //           this.selectedPlaces = place;
-            
+
   //         }
   //   )})
-          
-      
-      
+
   // }
-  }
+}
 
 
